5de1ecbcde1c0e15a0ab26682591266f098af877
marion petitprez <marion.petitprez@gmail.com>
2021-02-25 16:47:25 +0100

    exo Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts 'Creating Cocktails...'
+mojito = Cocktail.create!(name: "Mojito")
+bloody_mary = Cocktail.create!(name: "Bloody Mary")
+margarita = Cocktail.create!(name: "Margarita")
+puts 'Finished!'
+
+puts 'Creating Ingredients...'
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint_leaves = Ingredient.create!(name: "mint leaves")
+rhum = Ingredient.create!(name: "rhum")
+tomato_juice = Ingredient.create!(name: "tomate")
+tequila = Ingredient.create!(name: "tequila")
+vodka = Ingredient.create!(name: "vodka")
+puts 'Finished!'
+
+puts 'Creating Doses...'
+Dose.create!(description: "2 cl", cocktail: margarita, ingredient: tequila)
+Dose.create!(description: "6", cocktail: mojito, ingredient: mint_leaves)
+Dose.create!(description: "1.5 oz", cocktail: mojito, ingredient: rhum)
+Dose.create!(description: "4,5 cl", cocktail: bloody_mary, ingredient: vodka)
+Dose.create!(description: "9 cl", cocktail: bloody_mary, ingredient: tomato_juice)
+puts 'Finished!'
